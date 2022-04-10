1759d2828ed431867c667a8b55c4d802b50552b2
stckoverflw <stckoverflw@gmail.com>
2022-04-10 23:47:44 +0200

    make category case insensitive

--- lib/install/sortMods.ts
+++ lib/install/sortMods.ts
@@ -3,10 +3,10 @@ import {Mod} from "../../types/modProfile";
 export function sortMods(mods: Mod[]) {
     const modsSorted = new Map<string, Mod[]>()
     mods.map(value => {
-        if (modsSorted.has(value.type)) {
-            modsSorted.get(value.type)!!.push(value)
+        if (modsSorted.has(value.type.toLowerCase())) {
+            modsSorted.get(value.type.toLowerCase())!!.push(value)
         } else {
-            modsSorted.set(value.type, [value])
+            modsSorted.set(value.type.toLowerCase(), [value])
         }
     })
     return modsSorted
